2179bf3ab2967c9d53d96b96f5500ebdfbb3b836
rpg49 <pandey.rpg49@gmail.com>
2021-08-13 09:43:19 +0530

    fixing comments

--- diffInDates.py
+++ diffInDates.py
@@ -3,10 +3,10 @@ from datetime import date
 d1 = datetime.datetime.today().strftime('%Y/%m/%d')
 print (d1)
 d2 = '2021/07/22'
-print (d2)
+print ('date2 = ',d2)
 
 date1 = datetime.datetime.strptime(d1, '%Y/%m/%d')
-print (date1)
+print ('date1 =',date1)
 date2 = datetime.datetime.strptime(d2, '%Y/%m/%d')
 diff = (date1-date2).days
-print(diff)
+print('difference between date1 and date2', diff)
